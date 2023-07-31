3ae123d5b0781ea405f2bf95bc3e27ca9e68d123
cwj705 <cwj705@naver.com>
2023-07-31 16:40:06 +0900

    homework

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -1,9 +1,10 @@
 
-public class HellowGit {
+public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("111111111111111111111111");
 		System.out.println("222222222222222222222222");
 		System.out.println("333333333333333333333333");
+		System.out.println("집에서 작업을 하였습니다");
 	}
 
 }
